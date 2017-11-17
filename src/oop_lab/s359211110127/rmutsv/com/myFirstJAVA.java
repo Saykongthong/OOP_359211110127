ca8a1733725c5b231d8fc9f3b920196d4290a2b4
Saykongthong <saykongthong.22@gmail.com>
2017-11-17 11:14:27 +0700

    Successfuly information data

--- src/oop_lab/s359211110127/rmutsv/com/myFirstJAVA.java
+++ src/oop_lab/s359211110127/rmutsv/com/myFirstJAVA.java
@@ -3,6 +3,9 @@ package oop_lab.s359211110127.rmutsv.com;
 public class myFirstJAVA {
     public static void main(String[] args) {
         System.out.println("Hello Wordl, I'm Pronsuda");
+        System.out.println("Information System");
+        System.out.println("Faculty of Mangement Technololgy");
+        System.out.println("rmutsv");
     }//main
 
 }//class
