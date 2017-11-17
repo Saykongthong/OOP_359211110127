30b78b7c454eb8db6d9b3b1acb5e6bdd9a90338d
Saykongthong <saykongthong.22@gmail.com>
2017-11-17 10:33:06 +0700

    Initial commit

---
+++ src/oop_lab/s359211110127/rmutsv/com/myFirstJAVA.java
@@ -0,0 +1,8 @@
+package oop_lab.s359211110127.rmutsv.com;
+
+public class myFirstJAVA {
+    public static void main(String[] args) {
+        System.out.println("Hello Wordl, I'm Pronsuda");
+    }//main
+
+}//class
