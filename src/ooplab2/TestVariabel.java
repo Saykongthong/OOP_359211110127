403d5a12f765ef6743cd02971ccb01ba48f09766
Saykongthong <saykongthong.22@gmail.com>
2017-11-24 10:20:08 +0700

    Test Variabel

--- src/ooplab2/TestVariabel.java
+++ src/ooplab2/TestVariabel.java
@@ -2,47 +2,44 @@ package ooplab2;
 
 public class TestVariabel {
     public static void main(String[] args) {
-     // Integer
+        // Integer
         int i;
         i =10;
-        System.out.print("i="+i);
+        System.out.println("i="+i);
         int j = 20;
-        System.out.print("j ="+j);
-        System.out.print("i+j+"+(i+j));
+        System.out.println("j ="+j);
+        System.out.println("i+j+"+(i+j));
 
         //Double
         double v = 15.15;
-        System.out.print(v);
-        System.out.print(v*10);
+        System.out.println(v);
+        System.out.println(v*10);
 
        //Character
         char c = 'T';
-        System.out.print(c);
+        System.out.println(c);
 
         //String
         String s = "Hello,World";
-        System.out.print(s);
+        System.out.println(s);
         String name = " I'm Pornsuda";
-        System.out.print(name);
+        System.out.println(name);
         String msg = s + " " + name;
-        System.out.print(msg);
+        System.out.println(msg);
 
         String msg2 = s.concat(" "+name);
-        System.out.print(msg2);
-        System.out.print(msg.length());
-        System.out.print(msg.toUpperCase());
-        System.out.print(msg.toLowerCase());
+        System.out.println(msg2);
+        System.out.println(msg.length());
+        System.out.println(msg.toUpperCase());
+        System.out.println(msg.toLowerCase());
 
-       // boolean ค่าความจริง ทดสอบเงื่อนไข
+       // boolean
         boolean b = true;
         boolean b1 = false;
-        System.out.print(b);
-        System.out.print(b1);
+        System.out.println(b);
+        System.out.println(b1);
 
 
     } // main
-
-
-
 
 }// class
