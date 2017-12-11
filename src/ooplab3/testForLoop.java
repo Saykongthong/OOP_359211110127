5f8785c6e5ccf86ba5fe34b8b0536eaf4a225470
LAB_COM-MT_1 <saykongthong.22@gmail.com>
2017-12-11 15:18:35 +0700

    Test For Loop

---
+++ src/ooplab3/testForLoop.java
@@ -0,0 +1,25 @@
+package ooplab3;
+
+public class testForLoop {
+    public static void main(String[] args) {
+       //for loop
+      // for (int i=1;i<=10;i++) {
+      //     System.out.println("Hello");
+      //     System.out.println("IS223");
+       //}
+       for (int i=1; i<=100;i++) {
+           if (i%2==0)
+           System.out.print(i + " ");
+       }
+
+        for (int i=1; i<=100;i++) {
+            if (i%3==0 || i%5==0 || i%7==0)
+                System.out.print(i + " ");
+        }
+        for (int i=1; i<=100;i++) {
+            if (i%3==0 && i%5==0 && i%7==0)
+                System.out.print(i + " ");
+        }
+
+    }//main
+}//class
