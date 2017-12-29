8f95e44700b0b58be181528e43fbfc845bd26cfd
LAB_COM-MT_1 <saykongthong.22@gmail.com>
2017-12-29 10:20:35 +0700

    StringCompare

---
+++ src/ooplap6/myStrToken.java
@@ -0,0 +1,18 @@
+package ooplap6;
+
+import java.util.StringTokenizer;
+
+public class myStrToken {
+    public static void main(String[] args) {
+    //StringTokenizer
+    String msg = "Hello Welcome MT RMUTSV 2017";
+    StringTokenizer myToken = new StringTokenizer(msg);
+    //counToken() การนับจำนวนคำ
+    System.out.println(myToken.countTokens());
+
+    while (myToken.hasMoreTokens()){
+        StringBuffer buffer = new StringBuffer(myToken.nextToken());
+        System.out.print(buffer.reverse()+" ");
+    }//while
+    }//main
+}//class
