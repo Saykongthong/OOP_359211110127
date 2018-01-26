748d8c5542b8f02da2ff5b2faee83995fffd7b44
LAB_COM-MT_1 <saykongthong.22@gmail.com>
2018-01-26 10:08:17 +0700

    Access to instant variable

--- src/ooplab7/Student.java
+++ src/ooplab7/Student.java
@@ -9,6 +9,8 @@ package ooplab7;
 //3.protected
 public class Student {
    // properties of Student class
+   public static int age;
+   public  String group;
    private String id;
    private String name;
    private String pid;
