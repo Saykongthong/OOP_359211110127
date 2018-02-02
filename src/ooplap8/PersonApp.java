e05c2009793111f061b277b534c0963764c7cf2b
LAB_COM-MT_1 <saykongthong.22@gmail.com>
2018-02-02 11:15:31 +0700

    PersonApp

--- src/ooplap8/PersonApp.java
+++ src/ooplap8/PersonApp.java
@@ -10,11 +10,19 @@ public class PersonApp {
                                     "STD00001",
                                     "Information System");
     System.out.println(student.toString());
-
     student.setName("Boy Saiyong"); //set คือการเปลี่ยนค่า
     System.out.println(student.getName());
     System.out.println(student.toString());
-
+    {
+    Employee employee = new Employee("1809900638256",
+                    "Pornsuda",
+                    "187/3 M.6",
+                    "Maie",
+                    "PPPPP",
+                    "30000",
+                    "ZZZZZZZ");
+            System.out.println(employee.toString());
+        }
 
 
 
