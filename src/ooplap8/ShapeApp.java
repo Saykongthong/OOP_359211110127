78646c645bbff46cae4071453f2f0bb448c15c88
LAB_COM-MT_1 <saykongthong.22@gmail.com>
2018-02-02 11:18:32 +0700

    PersonApp

--- src/ooplap8/ShapeApp.java
+++ src/ooplap8/ShapeApp.java
@@ -3,11 +3,11 @@ package ooplap8;
 public class ShapeApp {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(20, 30);
-        System.out.println(rectangle.findArea());
+        System.out.println("Area of rectangle: "+rectangle.findArea());
 
         {
             Circle circle = new Circle (15);
-            System.out.println(circle.findArea());
+            System.out.println("Area of Circle: "+circle.findArea());
         }
     }//main
 }//class
